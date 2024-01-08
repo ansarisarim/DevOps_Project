8eca2cecec81ea245134b5e3916fdfdef7c7a3ab
sarim <ansarisarim2@gmail.com>
2024-01-08 06:42:57 +0000

    there is some changees in the scripts

--- dev.tf
+++ dev.tf
@@ -18,7 +18,6 @@ resource "aws_security_group" "demo-sg" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
@@ -26,7 +25,6 @@ resource "aws_security_group" "demo-sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
