b5988aa16d03814b618320a8482cf3af352317f7
sarim <ansarisarim2@gmail.com>
2024-01-08 10:17:48 +0000

    new file

---
+++ terra.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create VPC
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.1.0/24"
+  tags = {
+    name = "dpp-vpc"
+  }
+}
+
+# Create Subnets
+resource "aws_subnet" "dpp-public-subnet-01" {
+  vpc_id                  = aws_vpc.dpp-vpc.id
+  cidr_block              = "10.1.1.0/25"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+  tags = {
+    Name = "dpp-public-subnet-01"
+  }
+}
+
+resource "aws_subnet" "dpp-public-subnet-02" {
+  vpc_id                  = aws_vpc.dpp-vpc.id
+  cidr_block              = "10.1.1.128/25"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1b"
+  tags = {
+    Name = "dpp-public-subnet-02"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  tags = {
+    Name = "dpp-igw"
+  }
+}
+
+# Create Route Table
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = aws_vpc.dpp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id
+  }
+}
+
+# Associate Subnets with Route Table
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+  subnet_id      = aws_subnet.dpp-public-subnet-01.id
+  route_table_id = aws_route_table.dpp-public-rt.id
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+  subnet_id      = aws_subnet.dpp-public-subnet-02.id
+  route_table_id = aws_route_table.dpp-public-rt.id
+}
+
+# Create Security Group
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.dpp-vpc.id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "demo" {
+  ami                    = "ami-0a0f1259dd1c90938"
+  instance_type          = "t2.micro"
+  key_name               = "abc.pem"
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  subnet_id              = aws_subnet.dpp-public-subnet-01.id  # Choose your preferred subnet
+}
+
