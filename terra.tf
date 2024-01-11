3e3bdd3a456b18fdc47c23e53a922c3f3ecd9321
sarim <ansarisarim2@gmail.com>
2024-01-11 13:15:32 +0530

    with user credentials

--- terra.tf
+++ terra.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIAT3TOLJWUDFHRP5UD"
+  secret_key = "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"
 }
 
 # Create VPC
@@ -73,6 +75,15 @@ resource "aws_security_group" "demo-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "jenkins-port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
